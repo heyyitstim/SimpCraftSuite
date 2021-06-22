f8219c9895bb4400526eeb18c8fa754c96e95c08
BuildTools <unconfigured@null.spigotmc.org>
2021-06-22 18:58:25 -0500

    Stuff

--- src/main/java/heyyitstim/l2dragon/Items/DragonDrops.java
+++ src/main/java/heyyitstim/l2dragon/Items/DragonDrops.java
@@ -1,4 +1,15 @@
 package heyyitstim.l2dragon.Items;
 
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDeathEvent;
+
 public class DragonDrops {
+
+    @EventHandler
+    public void onEnderDragonDeath(EntityDeathEvent e){
+        if(e.getEntity() instanceof EnderDragon){
+            //do whatever
+        }
+    }
 }
