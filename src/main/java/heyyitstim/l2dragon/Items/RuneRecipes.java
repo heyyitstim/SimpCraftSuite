e7db80d8e886123bb17e7c9294d8c83f5c342804
BuildTools <unconfigured@null.spigotmc.org>
2021-06-22 18:38:36 -0500

    Added recipes for all runes

--- src/main/java/heyyitstim/l2dragon/Items/RuneRecipes.java
+++ src/main/java/heyyitstim/l2dragon/Items/RuneRecipes.java
@@ -20,8 +20,22 @@ public class RuneRecipes {
     public void registerRecipes() {
         // Flight Rune
         ShapedRecipe flightRuneRecipe = createFlightRune();
+        // Strength Rune
+        ShapedRecipe strengthRuneRecipe = createStrengthRune();
+        // Night Vision Rune
+        ShapedRecipe nvRuneRecipe = createNvRune();
+        // Speed Rune
+        ShapedRecipe speedRuneRecipe = createSpeedRune();
+        // Jump Rune
+        ShapedRecipe jumpRuneRecipe = createJumpRune();
+        // Fire Resistance Rune
+        ShapedRecipe fireRuneRecipe = createFireRune();
+        // Water Breathing Rune
+        ShapedRecipe waterRuneRecipe = createWaterRune();
 
         Main.instance.getServer().addRecipe(flightRuneRecipe);
+        Main.instance.getServer().addRecipe(strengthRuneRecipe);
+        Main.instance.getServer().addRecipe(nvRuneRecipe);
     }
 
     private ShapedRecipe createFlightRune() {
@@ -32,8 +46,8 @@ public class RuneRecipes {
         metaFw.setDisplayName(ChatUtil.color("&bFlight Rune"));
         metaFw.setLore(Arrays.asList(
                 ChatUtil.color(""),
-                ChatUtil.color("&7&oApply this to a &cDragon Chestplate"),
-                ChatUtil.color("to fly like you're wearing an elytra!")));
+                ChatUtil.color("&7&oApply this to a &c&oDragon Chestplate"),
+                ChatUtil.color("&7&oto &b&ofly &7&olike you're wearing an elytra!")));
         flightRune.setItemMeta(metaFw);
 
         ShapedRecipe flightRuneRecipe = new ShapedRecipe(flightRune);
@@ -57,8 +71,8 @@ public class RuneRecipes {
         metaFw.setDisplayName(ChatUtil.color("&cStrength Rune"));
         metaFw.setLore(Arrays.asList(
                 ChatUtil.color(""),
-                ChatUtil.color("&7&oApply this to any &cDragon Armor"),
-                ChatUtil.color("to gain permanent Strength 1!")));
+                ChatUtil.color("&7&oApply this to any &c&oDragon Armor"),
+                ChatUtil.color("&7&oto gain permanent &c&oStrength 1&7&o!")));
         strengthRune.setItemMeta(metaFw);
 
         ShapedRecipe strengthRuneRecipe = new ShapedRecipe(strengthRune);
@@ -68,9 +82,134 @@ public class RuneRecipes {
                 "YXY");
 
         strengthRuneRecipe.setIngredient('X', Material.IRON_NUGGET); // Dragon Scales
-        strengthRuneRecipe.setIngredient('Z', Material.ELYTRA);
+        strengthRuneRecipe.setIngredient('Z', Material.DIAMOND_AXE);
         strengthRuneRecipe.setIngredient('Y', Material.POTION); // Strength Potions
 
         return strengthRuneRecipe;
+    }
+
+    private ShapedRecipe createNvRune() {
+        ItemStack nvRune = new ItemStack(Material.FIREWORK_STAR, 1);
+        FireworkEffectMeta metaFw = (FireworkEffectMeta) nvRune.getItemMeta();
+        FireworkEffect aa = FireworkEffect.builder().withColor(Color.YELLOW).build();
+        metaFw.setEffect(aa);
+        metaFw.setDisplayName(ChatUtil.color("&eNight Vision Rune"));
+        metaFw.setLore(Arrays.asList(
+                ChatUtil.color(""),
+                ChatUtil.color("&7&oApply this to a &c&oDragon Helmet"),
+                ChatUtil.color("&7&oto gain permanent &e&onight vision&7&o!")));
+        nvRune.setItemMeta(metaFw);
+
+        ShapedRecipe nvRuneRecipe = new ShapedRecipe(nvRune);
+        nvRuneRecipe.shape(
+                "YXY",
+                "XZX",
+                "YXY");
+
+        nvRuneRecipe.setIngredient('X', Material.IRON_NUGGET); // Dragon Scales
+        nvRuneRecipe.setIngredient('Z', Material.GOLDEN_CARROT);
+        nvRuneRecipe.setIngredient('Y', Material.POTION); // Nightvision Potions
+
+        return nvRuneRecipe;
+    }
+
+    private ShapedRecipe createSpeedRune() {
+        ItemStack speedRune = new ItemStack(Material.FIREWORK_STAR, 1);
+        FireworkEffectMeta metaFw = (FireworkEffectMeta) speedRune.getItemMeta();
+        FireworkEffect aa = FireworkEffect.builder().withColor(Color.TEAL).build();
+        metaFw.setEffect(aa);
+        metaFw.setDisplayName(ChatUtil.color("&3Speed Rune"));
+        metaFw.setLore(Arrays.asList(
+                ChatUtil.color(""),
+                ChatUtil.color("&7&oApply this to any &c&oDragon Armor"),
+                ChatUtil.color("&7&oto gain permanent &3&oSpeed 1&7&o!")));
+        speedRune.setItemMeta(metaFw);
+
+        ShapedRecipe speedRuneRecipe = new ShapedRecipe(speedRune);
+        speedRuneRecipe.shape(
+                "YXY",
+                "XZX",
+                "YXY");
+
+        speedRuneRecipe.setIngredient('X', Material.IRON_NUGGET); // Dragon Scales
+        speedRuneRecipe.setIngredient('Z', Material.SUGAR);
+        speedRuneRecipe.setIngredient('Y', Material.POTION); // Speed Potions
+
+        return speedRuneRecipe;
+    }
+
+    private ShapedRecipe createJumpRune() {
+        ItemStack jumpRune = new ItemStack(Material.FIREWORK_STAR, 1);
+        FireworkEffectMeta metaFw = (FireworkEffectMeta) jumpRune.getItemMeta();
+        FireworkEffect aa = FireworkEffect.builder().withColor(Color.LIME).build();
+        metaFw.setEffect(aa);
+        metaFw.setDisplayName(ChatUtil.color("&aJumping Rune"));
+        metaFw.setLore(Arrays.asList(
+                ChatUtil.color(""),
+                ChatUtil.color("&7&oApply this to &c&oDragon Boots"),
+                ChatUtil.color("&7&oto gain permanent &a&oJump Boost 1&7&o!")));
+        jumpRune.setItemMeta(metaFw);
+
+        ShapedRecipe jumpRuneRecipe = new ShapedRecipe(jumpRune);
+        jumpRuneRecipe.shape(
+                "YXY",
+                "XZX",
+                "YXY");
+
+        jumpRuneRecipe.setIngredient('X', Material.IRON_NUGGET); // Dragon Scales
+        jumpRuneRecipe.setIngredient('Z', Material.SUGAR);
+        jumpRuneRecipe.setIngredient('Y', Material.POTION); // Speed Potions
+
+        return jumpRuneRecipe;
+    }
+
+    private ShapedRecipe createFireRune() {
+        ItemStack fireRune = new ItemStack(Material.FIREWORK_STAR, 1);
+        FireworkEffectMeta metaFw = (FireworkEffectMeta) fireRune.getItemMeta();
+        FireworkEffect aa = FireworkEffect.builder().withColor(Color.ORANGE).build();
+        metaFw.setEffect(aa);
+        metaFw.setDisplayName(ChatUtil.color("&6Fire Resistance Rune"));
+        metaFw.setLore(Arrays.asList(
+                ChatUtil.color(""),
+                ChatUtil.color("&7&oApply this to any &c&oDragon Armor"),
+                ChatUtil.color("&7&oto gain permanent &6&oFire Resistance 1&7&o!")));
+        fireRune.setItemMeta(metaFw);
+
+        ShapedRecipe fireRuneRecipe = new ShapedRecipe(fireRune);
+        fireRuneRecipe.shape(
+                "YXY",
+                "XZX",
+                "YXY");
+
+        fireRuneRecipe.setIngredient('X', Material.IRON_NUGGET); // Dragon Scales
+        fireRuneRecipe.setIngredient('Z', Material.MAGMA_CREAM);
+        fireRuneRecipe.setIngredient('Y', Material.POTION); // Fire Res. Potions
+
+        return fireRuneRecipe;
+    }
+
+    private ShapedRecipe createWaterRune() {
+        ItemStack waterRune = new ItemStack(Material.FIREWORK_STAR, 1);
+        FireworkEffectMeta metaFw = (FireworkEffectMeta) waterRune.getItemMeta();
+        FireworkEffect aa = FireworkEffect.builder().withColor(Color.NAVY).build();
+        metaFw.setEffect(aa);
+        metaFw.setDisplayName(ChatUtil.color("&1Water Breathing Rune"));
+        metaFw.setLore(Arrays.asList(
+                ChatUtil.color(""),
+                ChatUtil.color("&7&oApply this to a &c&oDragon Helmet"),
+                ChatUtil.color("&7&oto gain permanent &1&oWater Breathing&7&o!")));
+        waterRune.setItemMeta(metaFw);
+
+        ShapedRecipe waterRuneRecipe = new ShapedRecipe(waterRune);
+        waterRuneRecipe.shape(
+                "YXY",
+                "XZX",
+                "YXY");
+
+        waterRuneRecipe.setIngredient('X', Material.IRON_NUGGET); // Dragon Scales
+        waterRuneRecipe.setIngredient('Z', Material.WATER_BUCKET);
+        waterRuneRecipe.setIngredient('Y', Material.TURTLE_HELMET); // Water Breathing Potions
+
+        return waterRuneRecipe;
     }
 }
--- src/main/java/heyyitstim/l2dragon/Main.java
+++ src/main/java/heyyitstim/l2dragon/Main.java
@@ -1,6 +1,7 @@
 package heyyitstim.l2dragon;
 
 import heyyitstim.l2dragon.Items.ArmorRecipes;
+import heyyitstim.l2dragon.Items.RuneRecipes;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -11,6 +12,7 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         instance = this;
         new ArmorRecipes();
+        new RuneRecipes();
         System.out.println("L2Dragon has been enabled!");
     }
 
