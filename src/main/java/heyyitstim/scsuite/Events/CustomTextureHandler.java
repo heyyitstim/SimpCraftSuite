5672a08824514e2304bc1973f98aa527eb9ba0f5
BuildTools <unconfigured@null.spigotmc.org>
2022-05-28 16:44:34 -0500

    Updated resource pack link

--- src/main/java/heyyitstim/scsuite/Events/CustomTextureHandler.java
+++ src/main/java/heyyitstim/scsuite/Events/CustomTextureHandler.java
@@ -8,6 +8,6 @@ public class CustomTextureHandler implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        e.getPlayer().setResourcePack("https://www.dropbox.com/s/22w8k0oijw9nc1x/SimpCraft%20v1.2.1.zip?dl=1"); // resource pack link
+        e.getPlayer().setResourcePack("https://www.dropbox.com/s/003q8gt87zmasee/SimpCraft%20v2.0.zip?dl=1"); // resource pack link
     }
 }
